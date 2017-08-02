78702a33222854c5964812b98affc5a5da8c4e21
Lynn Josse <lynnjosse@sbcglobal.net>
2017-08-02 16:36:34 -0500

    added dependencies and cleaned up models

--- src/main/java/com/lynnjosse/EvalTool/models/User.java
+++ src/main/java/com/lynnjosse/EvalTool/models/User.java
@@ -14,23 +14,47 @@ import java.util.List;
 public class User extends AbstractEntity {
 
     @NotNull
-    @Pattern(regexp = "[a-zA-Z][a-zA-Z0-9_-]{4,11}", message = "Invalid username")
+    @Pattern(regexp = "[a-zA-Z][a-zA-Z0-9_-]{4,20}", message = "Invalid username")
     private String username;
 
     @NotNull
     private String pwHash;
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
     public User() {}
 
-    public User(String username, String password) {
+    public User(String username, String password, String email, String firstName, String lastName) {
         this.username = username;
         this.pwHash = hashPassword(password);
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) {  this.firstName = firstName;  }
+
+    public String getLastName() { return lastName;}
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
     public String getUsername() {
         return username;
     }
+
+    public String getEmail() { return email;}
+
+    public void setEmail(String email) { this.email = email;}
 
     private static String hashPassword(String password) {
         return encoder.encode(password);
@@ -39,5 +63,4 @@ public class User extends AbstractEntity {
     public boolean isMatchingPassword(String password) {
         return encoder.matches(password, pwHash);
     }
-
 }
