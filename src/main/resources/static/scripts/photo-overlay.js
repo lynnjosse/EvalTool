569b1efea69c8cb5f9e4f6746b7fff8d39b4e516
Lynn Josse <lynnjosse@sbcglobal.net>
2017-12-14 18:01:04 -0600

    click to get out of lightbox

--- src/main/resources/static/scripts/photo-overlay.js
+++ src/main/resources/static/scripts/photo-overlay.js
@@ -1,2 +1,13 @@
-var $overlay = $('<div id = "overlay"></div>');
-var $image = $("");
+document.getElementById("evalphotodiv").onclick = function() {lightBox()};
+
+var bigPhoto = document.getElementById("overlay");
+
+function lightBox() {
+    bigPhoto.style.display = "block";
+}
+
+document.getElementById("overlay").onclick =function() {unLightBox()};
+
+function unLightBox() {
+    bigPhoto.style.display = "none";
+    }
